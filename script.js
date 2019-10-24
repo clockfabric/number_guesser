9a1b62e2cf8d75683505c6a3ecd30fe71788ea6e
Clock Fabric <25046826+clockfabric@users.noreply.github.com>
2019-10-24 15:34:38 -0400

    First commit!

---
+++ script.js
@@ -0,0 +1,46 @@
+let number = Math.floor(Math.random() * 100) + 1;
+//console.log("Secret number: " + number);
+
+let numberOfGuesses = 0;
+
+let inputNumber = document.querySelector("#input_number");
+let messageAlert = document.querySelector(".message");
+let guesses = document.querySelector(".guesses");
+
+let guessButton = document.querySelector(".guess_button");
+guessButton.addEventListener("click", () => {
+	let guessedNumber = inputNumber.value;
+
+	if (isNaN(guessedNumber)) {
+		messageAlert.innerHTML = "Enter a valid number!";
+	} else {
+		if (guessedNumber < 1 || guessedNumber > 100) {
+			messageAlert.innerHTML = "Enter a number between 1 and 100";
+		} else {
+			if (guessedNumber < number) {
+				messageAlert.innerHTML = "Your guess is too small!";
+			} else if (guessedNumber > number) {
+				messageAlert.innerHTML = "Your guess is too big!";
+			} else {
+				messageAlert.classList.add("winning_message");
+				messageAlert.innerHTML =
+					"Your guess of " + guessedNumber + " is correct!";
+				guessButton.innerText = "Refresh to play again!";
+				guessButton.disabled = true;
+			}
+			let guessItem = document.createElement("span");
+			guessItem.classList.add("guess");
+			guessItem.innerText = guessedNumber.toString();
+			guesses.appendChild(guessItem);
+
+			if (numberOfGuesses >= 9) {
+				messageAlert.innerHTML = "Sorry you ran out of 10 guesses!!!";
+				guessButton.innerText = "Refresh to play again!";
+				guessButton.disabled = true;
+			}
+			numberOfGuesses++;
+		}
+	}
+
+	//console.log(guessedNumber);
+});
